18f1b86eb0bb9b1728bb1707cd6f1881cbcaea84
Yu Jun Koh <yjenexd@gmail.com>
2026-06-16 11:13:12 +0800

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,26 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = collections.defaultdict(list) ##initialize a dict
+        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append((timestamp, value))
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.store.get(key, [])
+        if not values:
+            return ""
+        low = 0
+        high = len(values)
+        res = ""
+        while low < high:
+            mid = low + (high - low) // 2
+            if values[mid][0] <= timestamp:
+                res = values[mid][1]
+                low = mid + 1
+            else:
+                high = mid
+        return res
+
+        ##binary search requires sorted list, cannot do on hashmap as you cannot sort a hashmap
